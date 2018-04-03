62e439e89d24fa0f84492ca7ada982439ed56277
mac <mac@macs-Device.local>
2018-04-03 00:35:16 -0700

    make it better

--- src/components/constants/server.js
+++ src/components/constants/server.js
@@ -1,5 +1,5 @@
 const BACKEND_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'https://campusbarter.herokuapp.com'
+  ? 'https://campusbartertwo.herokuapp.com'
   : 'http://localhost:3001';
 
 export default BACKEND_SERVER_URL;
