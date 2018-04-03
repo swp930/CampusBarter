79a7a9a3d9b52814782746e0b62937994739d980
mac <mac@macs-Device.local>
2018-04-03 01:08:47 -0700

    Making it better

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ class Login extends React.Component {
   responseGoogle(response) {
     Auth.authenticateUser(response)
     console.log(response)
-    window.location.reload();
+    //window.location.reload();
   }
   render(){
     return(
