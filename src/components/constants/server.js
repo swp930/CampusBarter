4762e8b58ac3528db73d75890ed0501c931d2fc7
mac <mac@macs-Device.local>
2018-04-03 00:50:01 -0700

    Start with create-react-app

--- src/components/constants/server.js
+++ src/components/constants/server.js
@@ -1,5 +1,5 @@
 const BACKEND_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'https://campusbartertwo.herokuapp.com'
+  ? 'https://campusbarter.herokuapp.com'
   : 'http://localhost:3001';
 
 export default BACKEND_SERVER_URL;
