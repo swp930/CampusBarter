ad079f489af1a99fa806402f18d89d303c69e52c
swp930 <swp930@gmail.com>
2025-10-27 15:08:45 -0700

    Remove Procfile

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,24 +1,24 @@
 import React from 'react'
 
-class Message extends React.Component{
-  constructor(props){
+class Message extends React.Component {
+  constructor(props) {
     super(props)
     this.norm = this.norm.bind(this)
   }
-  norm(){
+  norm() {
     this.props.onNormal(this.props.message, this.props.sender)
   }
   render() {
-    return(
+    return (
       <div className="col-md-4">
         <div className="card card-body">
           <h4 className="card-title">New message from
             <i><small> {this.props.sender}</small></i>
           </h4>
           <p className="card-text">{this.props.message}</p>
-          <a href="#" className="btn btn-success" data-toggle="modal" data-target="#acceptModal" onClick={this.norm}>Accept</a>
+          <a className="btn btn-success" data-toggle="modal" data-target="#acceptModal" onClick={this.norm}>Accept</a>
           <div className="space"></div>
-          <a href="#" className="btn btn-danger" data-toggle="modal" data-target="#declineModal" onClick={this.norm}>Decline</a>
+          <a className="btn btn-danger" data-toggle="modal" data-target="#declineModal" onClick={this.norm}>Decline</a>
         </div>
       </div>
     )
